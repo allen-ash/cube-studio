c312fa160dc0cd536d71826f95d0328de67acbe0
pengluan <pengluan@tencent.com>
2022-07-26 17:34:59 +0800

    详情字段顺序

--- job-template/job/horovod/start.py
+++ job-template/job/horovod/start.py
@@ -113,12 +113,6 @@ def make_mpijob(name):
                         },
                         "spec": {
                             "volumes": k8s_volumes,
-                            "imagePullSecrets": [
-                                {
-                                    "name": "hubsecret"
-                                }
-                            ],
-
                             "containers": [
                                 {
                                     "image": WORK_IMAGES,
